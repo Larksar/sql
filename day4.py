ca6b8d751a0879bb60341410f54d3d0f84c2202a
Larksar <angelouk.m30@gmail.com>
2026-07-24 20:01:51 +0100

    day6

--- day4.py
+++ day4.py
@@ -1,2 +1,15 @@
 def function(day):
     print(f"It is the {day}")
+
+
+class sql:
+
+    def __init__(self, employee_id, hourly_pay):
+
+        self.employee_id = employee_id,
+        self.hourly_pay = hourly_pay
+
+    def get_info(self):
+        return f"{self.employee_id}'s hourly pay is {self.hourly_pay}"
+
+
